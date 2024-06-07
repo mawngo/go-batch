bed2366f254845f6f714f56eae9f8ed64fad8ce6
Toannd <toannd@eway.vn>
2024-06-07 12:10:53 +0700

    test: add test for batch processor

---
+++ batch_test.go
@@ -0,0 +1,511 @@
+package batch
+
+import (
+	"context"
+	"errors"
+	"go.uber.org/goleak"
+	"log/slog"
+	"math/rand"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m,
+		// This test will leak as the processor is forced to close before task completed, which was an expected behaviour.
+		goleak.IgnoreAnyFunction("github.com/mawngo/go-batch.TestCloseContext.func1"),
+		goleak.IgnoreAnyFunction("github.com/mawngo/go-batch.TestClusterCloseContext.func2"),
+	)
+}
+
+func TestBatched(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedSplit(t *testing.T) {
+	maxes := []int{1, 2, 5, 8, 10, 18, 20, 111}
+	for _, m := range maxes {
+		sum := int32(0)
+		processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+			Configure(WithMaxItem(10)).
+			WithSplitter(SplitSliceEqually[int](m)).
+			Run(summing(&sum))
+
+		for i := 0; i < 100_000; i++ {
+			processor.Put(1)
+		}
+		processor.MustClose()
+		if sum != 100_000 {
+			t.Fatalf("sum is %d != 100_000", sum)
+		}
+	}
+}
+
+func TestBatchedAllDisabled(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(Disabled), WithMaxWait(Disabled)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestConcurrentBatchedAllDisabled(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(Disabled), WithMaxWait(Disabled), WithMaxConcurrency(10)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedAggressive(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithAggressiveMode()).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestConcurrentBatchedAggressive(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithAggressiveMode(), WithMaxConcurrency(10)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestDisabled(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(0)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestCloseContext(t *testing.T) {
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(5), WithMaxConcurrency(Unlimited)).
+		Run(func(_ []int, _ int64) error {
+			time.Sleep(100 * time.Second)
+			return nil
+		})
+
+	for i := 0; i < 10; i++ {
+		processor.Put(1)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err := processor.CloseContext(ctx)
+	if err == nil {
+		t.Fatalf("processor is closed before complete processing!")
+	}
+}
+
+func TestDrainContext(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(11), WithBlockWhileProcessing(), WithMaxWait(Disabled)).
+		Run(summing(&sum))
+
+	for i := 0; i < 10; i++ {
+		processor.Put(1)
+	}
+	if sum != 0 {
+		t.Fatalf("item processed while limit not reached")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err := processor.DrainContext(ctx)
+	if err != nil {
+		t.Fatalf("drain result in an error %v", err)
+	}
+	if sum != 10 {
+		t.Fatalf("drain not processing remaining item")
+	}
+	processor.MustClose()
+}
+
+func TestFlushContext(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(11), WithBlockWhileProcessing(), WithMaxWait(Disabled)).
+		Run(summing(&sum))
+
+	for i := 0; i < 10; i++ {
+		processor.Put(1)
+	}
+	if sum != 0 {
+		t.Fatalf("item processed while limit not reached")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err := processor.FlushContext(ctx)
+	if err != nil {
+		t.Fatalf("flush result in an error %v", err)
+	}
+	if sum != 10 {
+		t.Fatalf("flush not processing remaining item")
+	}
+	processor.MustClose()
+}
+
+func TestBatchedDefault(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedBlockWhileProcessing(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithBlockWhileProcessing(), WithMaxItem(10)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedConcurrentBlockWhileProcessing(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithBlockWhileProcessing(), WithMaxItem(10), WithMaxConcurrency(10)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedConcurrent(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxConcurrency(10), WithMaxItem(10)).
+		Run(summing(&sum))
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedNoWait(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithMaxWait(0)).
+		Run(summing(&sum))
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func TestBatchedWait(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithMaxWait(500*time.Millisecond)).
+		Run(summing(&sum))
+	for i := 0; i < 9; i++ {
+		processor.Put(1)
+	}
+	time.Sleep(1 * time.Second)
+	if processor.ItemCount() != 0 {
+		t.Fatalf("item is not processed after timeout passed")
+	}
+	if sum != 9 {
+		t.Fatalf("sum is %d != 9", sum)
+	}
+	processor.MustClose()
+}
+
+func TestBatchedSoftWait(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxWait(500 * time.Millisecond)).
+		Run(func(ints []int, _ int64) error {
+			if len(ints) > 0 {
+				t.Fatalf("TestBatchedSoftWait must not put any item to processor")
+			}
+			atomic.AddInt32(&sum, 1)
+			return nil
+		})
+	time.Sleep(1 * time.Second)
+	if sum != 0 {
+		t.Fatalf("soft wait process still run even when the batch is empty")
+	}
+	processor.MustClose()
+}
+
+func TestBatchedHardWait(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithHardMaxWait(500 * time.Millisecond)).
+		Run(func(ints []int, _ int64) error {
+			if len(ints) > 0 {
+				t.Fatalf("TestBatchedHardWait must not put any item to processor")
+			}
+			atomic.AddInt32(&sum, 1)
+			return nil
+		})
+	time.Sleep(1 * time.Second)
+	if sum == 0 {
+		t.Fatalf("hard wait process not run when the batch is empty")
+	}
+	processor.MustClose()
+}
+
+func TestBatchedConcurrentNoWait(t *testing.T) {
+	sum := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxConcurrency(10), WithMaxItem(10), WithMaxWait(0)).
+		Run(summing(&sum))
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum)
+	}
+}
+
+func summing(p *int32) ProcessBatchFn[[]int] {
+	return func(ints []int, _ int64) error {
+		for _, num := range ints {
+			atomic.AddInt32(p, int32(num))
+		}
+		return nil
+	}
+}
+
+func TestBasicError(t *testing.T) {
+	sum := int32(0)
+	errCnt := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(Disabled)).
+		Run(summingErr(&sum), func(ints []int, i int64, _ error) error {
+			slog.Info("receive error of", slog.Any("ints", ints))
+			atomic.AddInt32(&errCnt, int32(i))
+			return nil
+		})
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum+errCnt != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum+errCnt)
+	}
+}
+
+func TestRemainError(t *testing.T) {
+	sum := int32(0)
+	errCnt := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(Disabled)).
+		Run(summingErrHalf(&sum), func(ints []int, i int64, _ error) error {
+			slog.Info("receive error of", slog.Any("ints", ints))
+			atomic.AddInt32(&errCnt, int32(i))
+			return nil
+		})
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum+(errCnt*2) != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum+(errCnt*2))
+	}
+}
+
+func TestRemainErrorNoMaxWait(t *testing.T) {
+	sum := int32(0)
+	errCnt := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(100), WithDisabledDefaultProcessErrorLog()).
+		Run(summingErrHalfSleep(&sum, 200*time.Millisecond), func(ints []int, i int64, _ error) error {
+			slog.Info("receive error of", slog.Any("ints", ints))
+			atomic.AddInt32(&errCnt, int32(i))
+			return nil
+		})
+
+	for i := 0; i < 1_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum+(errCnt*2) != 1_000 {
+		t.Fatalf("sum is %d != 1_000", sum+(errCnt*2))
+	}
+}
+
+func TestRemainErrorChain(t *testing.T) {
+	sum := int32(0)
+	errCnt := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(Disabled)).
+		Run(summingErr(&sum),
+			func(ints []int, i int64, _ error) error {
+				slog.Info("receive error of", slog.Any("ints", ints))
+				half := i / 2
+				atomic.AddInt32(&errCnt, int32(half))
+				return NewErrorWithRemaining(errors.New("continue"), ints[0:half], half)
+			},
+			func(ints []int, count int64, _ error) error {
+				slog.Info("receive error of", slog.Any("ints", ints))
+				for i := int64(0); i < count; i++ {
+					atomic.AddInt32(&errCnt, int32(ints[i]))
+				}
+				return nil
+			},
+			func(_ []int, _ int64, _ error) error {
+				t.Fatalf("should stopped")
+				return nil
+			},
+		)
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum+errCnt != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum+errCnt)
+	}
+}
+
+func TestBasicErrorChain(t *testing.T) {
+	sum := int32(0)
+	errCnt := int32(0)
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(10), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(Disabled)).
+		Run(summingErr(&sum),
+			func(ints []int, i int64, _ error) error {
+				slog.Info("receive error of", slog.Any("ints", ints))
+				atomic.AddInt32(&errCnt, int32(i))
+				return errors.New("continue")
+			},
+			func(ints []int, count int64, _ error) error {
+				slog.Info("receive error of", slog.Any("ints", ints))
+				for i := int64(0); i < count; i++ {
+					atomic.AddInt32(&errCnt, int32(ints[i]))
+				}
+				return nil
+			},
+			func(_ []int, _ int64, _ error) error {
+				t.Fatalf("should stopped")
+				return nil
+			})
+
+	for i := 0; i < 1_000_000; i++ {
+		processor.Put(1)
+	}
+	processor.MustClose()
+	if sum+(errCnt/2) != 1_000_000 {
+		t.Fatalf("sum is %d != 1_000_000", sum+(errCnt/2))
+	}
+}
+
+func summingErrHalf(p *int32) ProcessBatchFn[[]int] {
+	return func(ints []int, i int64) error {
+		if rand.Intn(10) > 6 {
+			half := i / 2
+			return NewErrorWithRemaining(errors.New("random error"), ints[0:half], half)
+		}
+		for _, num := range ints {
+			atomic.AddInt32(p, int32(num))
+		}
+		return nil
+	}
+}
+
+func summingErrHalfSleep(p *int32, sleep time.Duration) ProcessBatchFn[[]int] {
+	return func(ints []int, i int64) error {
+		time.Sleep(sleep)
+		if rand.Intn(10) > 6 {
+			half := i / 2
+			return NewErrorWithRemaining(errors.New("random error"), ints[0:half], half)
+		}
+		for _, num := range ints {
+			atomic.AddInt32(p, int32(num))
+		}
+		return nil
+	}
+}
+
+func summingErr(p *int32) ProcessBatchFn[[]int] {
+	return func(ints []int, _ int64) error {
+		if rand.Intn(10) > 6 {
+			return errors.New("random error")
+		}
+		for _, num := range ints {
+			atomic.AddInt32(p, int32(num))
+		}
+		return nil
+	}
+}
---
+++ builtin.go
@@ -0,0 +1,126 @@
+package batch
+
+// InitSlice is InitBatchFn that allocate a slice.
+func InitSlice[T any](i int64) []T {
+	if i < 0 {
+		return make([]T, 0)
+	}
+	if i == 0 {
+		return make([]T, 1)
+	}
+	return make([]T, 0, i)
+}
+
+// AddToSlice is MergeToBatchFn that add item to a slice.
+func AddToSlice[T any](b []T, item T) []T {
+	return append(b, item)
+}
+
+// InitMap is InitBatchFn that allocate a map.
+func InitMap[K comparable, V any](i int64) map[K]V {
+	if i == 0 {
+		return make(map[K]V, 1)
+	}
+	return make(map[K]V)
+}
+
+type Extractor[T any, V any] func(T) V
+type Combine[T any] func(T, T) T
+
+// AddToMapUsing create MergeToBatchFn that add item to map using key and value Extractor.
+func AddToMapUsing[T any, K comparable, V any](keyExtractor Extractor[T, K], valueExtractor Extractor[T, V]) MergeToBatchFn[map[K]V, T] {
+	return func(m map[K]V, t T) map[K]V {
+		key := keyExtractor(t)
+		value := valueExtractor(t)
+		m[key] = value
+		return m
+	}
+}
+
+// AddSelfToMapUsing create MergeToBatchFn that add self as item to map using key Extractor.
+func AddSelfToMapUsing[T any, K comparable](keyExtractor Extractor[T, K]) MergeToBatchFn[map[K]T, T] {
+	return func(m map[K]T, t T) map[K]T {
+		key := keyExtractor(t)
+		m[key] = t
+		return m
+	}
+}
+
+// MergeToMapUsing create MergeToBatchFn that add item to map using key and value Extractor and apply Combine if key duplicated.
+// Original value will be passed as 1st parameter to Combine function.
+func MergeToMapUsing[T any, K comparable, V any](keyExtractor Extractor[T, K], valueExtractor Extractor[T, V], combiner Combine[V]) MergeToBatchFn[map[K]V, T] {
+	return func(m map[K]V, t T) map[K]V {
+		key := keyExtractor(t)
+		value := valueExtractor(t)
+		if v, ok := m[key]; ok {
+			m[key] = combiner(v, value)
+			return m
+		}
+		m[key] = value
+		return m
+	}
+}
+
+// MergeSelfToMapUsing create MergeToBatchFn that add self as item to map using key Extractor and apply Combine if key duplicated.
+// Original value will be passed as 1st parameter to Combine function.
+func MergeSelfToMapUsing[T any, K comparable](keyExtractor Extractor[T, K], combiner Combine[T]) MergeToBatchFn[map[K]T, T] {
+	return func(m map[K]T, t T) map[K]T {
+		key := keyExtractor(t)
+		if v, ok := m[key]; ok {
+			m[key] = combiner(v, t)
+			return m
+		}
+		m[key] = t
+		return m
+	}
+}
+
+// InitChan is InitBatchFn that allocate a channel.
+// this should not be used with unbounded processor (maxItem < 0).
+func InitChan[T any](i int64) chan T {
+	if i < 0 {
+		panic("cannot use unbounded processor with channel batch")
+	}
+	if i == 0 {
+		return make(chan T, 1)
+	}
+	return make(chan T, i)
+}
+
+// InitType is InitBatchFn that allocate a type T.
+func InitType[T any](_ int64) T {
+	t := new(T)
+	return *t
+}
+
+// SplitSliceEqually create a SplitBatchFn that split a slice into multiple equally chuck.
+func SplitSliceEqually[T any, I Size](numberOfChunk I) SplitBatchFn[[]T] {
+	return func(b []T, _ int64) [][]T {
+		batches := make([][]T, numberOfChunk)
+		for i := 0; i < len(b); i++ {
+			bucket := int64(i) % int64(numberOfChunk)
+			batches[bucket] = append(batches[bucket], b[i])
+		}
+		return batches
+	}
+}
+
+// SplitSliceSizeLimit create a SplitBatchFn that split a slice into multiple chuck of limited size.
+func SplitSliceSizeLimit[T any, I Size](maxSizeOfChunk I) SplitBatchFn[[]T] {
+	return func(b []T, i int64) [][]T {
+		size := i / int64(maxSizeOfChunk)
+		if i%int64(maxSizeOfChunk) != 0 {
+			size++
+		}
+		batches := make([][]T, size)
+		index := 0
+		for batchI := 0; batchI < len(batches); batchI++ {
+			batch := batches[batchI]
+			for ; index < len(b); index++ {
+				batch = append(batch, b[index])
+			}
+			batches[batchI] = batch
+		}
+		return batches
+	}
+}
