b552e3a4383d1b7f5aabd5c52751cfc62105c62c
mawngo <sitdownrightnow.dev@gmail.com>
2025-05-12 18:45:11 +0700

    bench: update benchmark result, add PutAll benchmark.

--- BENCHMARK.md
+++ BENCHMARK.md
@@ -16,8 +16,12 @@ goos: windows
 goarch: amd64
 pkg: github.com/mawngo/go-batch
 cpu: AMD Ryzen 9 7900 12-Core Processor
-BenchmarkPutFree/put-24         44923292                24.30 ns/op           41 B/op          0 allocs/op
-BenchmarkPutContextFree/put-24          36917396                30.97 ns/op           40 B/op          0 allocs/op
+BenchmarkPut1Free/put-24                44828139                24.23 ns/op           41 B/op          0 allocs/op
+BenchmarkPutContext1Free/put-24         34623241                31.20 ns/op           43 B/op          0 allocs/op
+BenchmarkPut3Free/put-24                16271760                70.97 ns/op          144 B/op          0 allocs/op
+BenchmarkPutAll3Free/put-24             34782003                32.34 ns/op          131 B/op          0 allocs/op
+BenchmarkPutContext3Free/put-24         11623908                95.73 ns/op          129 B/op          0 allocs/op
+BenchmarkPutAllContext3Free/put-24              27978288                39.24 ns/op          131 B/op          0 allocs/op
 PASS
-ok      github.com/mawngo/go-batch      2.446s
+ok      github.com/mawngo/go-batch      6.920s
 ```
--- batch_bench_test.go
+++ batch_bench_test.go
@@ -6,10 +6,10 @@ import (
 	"time"
 )
 
-func BenchmarkPutFree(b *testing.B) {
+func BenchmarkPut1Free(b *testing.B) {
 	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
 		Configure(WithMaxItem(Unlimited), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(100*time.Hour)).
-		Run(func(ints []int, _ int64) error {
+		Run(func(_ []int, _ int64) error {
 			return nil
 		})
 	b.Run("put", func(b *testing.B) {
@@ -20,16 +20,78 @@ func BenchmarkPutFree(b *testing.B) {
 	processor.MustClose()
 }
 
-func BenchmarkPutContextFree(b *testing.B) {
+func BenchmarkPutContext1Free(b *testing.B) {
 	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
 		Configure(WithMaxItem(Unlimited), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(100*time.Hour)).
-		Run(func(ints []int, _ int64) error {
+		Run(func(_ []int, _ int64) error {
 			return nil
 		})
 	ctx := context.Background()
 	b.Run("put", func(b *testing.B) {
 		for b.Loop() {
 			processor.PutContext(ctx, 1)
+		}
+	})
+	processor.MustClose()
+}
+
+func BenchmarkPut3Free(b *testing.B) {
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(Unlimited), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(100*time.Hour)).
+		Run(func(_ []int, _ int64) error {
+			return nil
+		})
+	b.Run("put", func(b *testing.B) {
+		for b.Loop() {
+			processor.Put(1)
+			processor.Put(1)
+			processor.Put(1)
+		}
+	})
+	processor.MustClose()
+}
+
+func BenchmarkPutAll3Free(b *testing.B) {
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(Unlimited), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(100*time.Hour)).
+		Run(func(_ []int, _ int64) error {
+			return nil
+		})
+	b.Run("put", func(b *testing.B) {
+		for b.Loop() {
+			processor.PutAll([]int{1, 1, 1})
+		}
+	})
+	processor.MustClose()
+}
+
+func BenchmarkPutContext3Free(b *testing.B) {
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(Unlimited), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(100*time.Hour)).
+		Run(func(_ []int, _ int64) error {
+			return nil
+		})
+	ctx := context.Background()
+	b.Run("put", func(b *testing.B) {
+		for b.Loop() {
+			processor.PutContext(ctx, 1)
+			processor.PutContext(ctx, 1)
+			processor.PutContext(ctx, 1)
+		}
+	})
+	processor.MustClose()
+}
+
+func BenchmarkPutAllContext3Free(b *testing.B) {
+	processor := NewProcessor(InitSlice[int], AddToSlice[int]).
+		Configure(WithMaxItem(Unlimited), WithBlockWhileProcessing(), WithDisabledDefaultProcessErrorLog(), WithMaxWait(100*time.Hour)).
+		Run(func(_ []int, _ int64) error {
+			return nil
+		})
+	ctx := context.Background()
+	b.Run("put", func(b *testing.B) {
+		for b.Loop() {
+			processor.PutAllContext(ctx, []int{1, 1, 1})
 		}
 	})
 	processor.MustClose()
